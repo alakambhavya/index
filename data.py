c87f6de258caf6e46a24185914c563c3531afca7
alakambhavya <alakambhavya@gmail.com>
2026-05-19 23:07:22 +0530

    explaining index values

---
+++ data.py
@@ -0,0 +1,13 @@
+data=1,2,3,4,5,6,8,9,"1234567",(1,2,4,5,6,"Bhavya","12",["Bhavya is very bad girl",{1,2,4}],"She is topper in the class",("She","Got",99,"percentage"),"VJISS")
+print(data[9][9][2])
+dec=99
+print(dec)
+octal=oct(dec)
+print(octal)
+print(data[9][5][2])
+print(data[9][8][12])
+print(data[9][6])
+print(data[9][8][4])
+print(data[9][10][2])
+print(data[9][7][1])
+
